c09ce46d46cec689dac12ef07e62bd4ee327cf55
Mike Lysikov <d8corp@mail.ru>
2023-01-28 13:10:01 +0300

    update fe template

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,21 +1,35 @@
 import { JsxComponent } from '@innet/jsx'
-import { State } from 'watch-state'
+import { Cache, State } from 'watch-state'
 
 import styles from './HomePage.scss'
 import logo from './logo.svg'
 
 const name = new State('World')
+const world = new Cache(() => name.value
+  ? `, ${name.value}`
+  : '',
+)
+
+const handleInput = (e: Event) => {
+  const target = e.target as HTMLInputElement
+  name.value = target.value
+}
 
 export const HomePage: JsxComponent = () => (
   <div class={styles.root}>
-    <img src={logo} width={300} style='margin: 0 auto;' alt='logo' />
+    <img
+      src={logo}
+      width={300}
+      style={{ margin: '0 auto' }}
+      alt='innet'
+    />
     <h1 class={styles.header}>
-      Hello{() => name.value ? `, ${name.value}` : ''}!
+      Hello{world}!
     </h1>
     <input
       class={styles.input}
-      value={() => name.value}
-      oninput={e => (name.value = e.target.value)}
+      value={name}
+      oninput={handleInput}
       placeholder='Enter your name'
     />
   </div>
