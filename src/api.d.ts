6223863735b5d5c90317a29c5499ccf6ef809609
Deight <d8corp@mail.ru>
2026-07-01 14:43:39 +0300

    update api template

--- src/api.d.ts
+++ src/api.d.ts
@@ -3,62 +3,62 @@ import '@innet/server'
 declare global {
   namespace Api {
     export interface Schemas {
-    TodoAdd: {
-      id: string
-      title: string
-      done: boolean
-    }
-    Todo: {
-      id: string
-      title: string
-      done: boolean
-    }
-  }
-  export interface Endpoints {
-    ['GET:/todos']: {
-      search: {
-        done?: boolean
-        page: number
-        pageSize: number
+      TodoAdd: {
+        'id': string
+        'title': string
+        'done': boolean
       }
-      response: {
-        ['default']: {
-          page: number
-          pageSize: number
-          count: number
-          todos: Array<Schemas['Todo']>
+      Todo: {
+        'id': string
+        'title': string
+        'done': boolean
+      }
+    }
+    export interface Endpoints {
+      'GET:/todos': {
+        search: {
+          'done'?: boolean
+          'page': number
+          'pageSize': number
         }
-     }
-    }
-    ['POST:/todos']: {
-      body: Schemas['TodoAdd']
-      response: {
-        ['204']: void
-     }
-    }
-    ['GET:/todos/{todoId}']: {
-      params: {
-        todoId: string
+        response: {
+          'default': {
+            'page': number
+            'pageSize': number
+            'count': number
+            'todos': Array<Schemas['Todo']>
+          }
+        }
       }
-      response: {
-        ['default']: Schemas['Todo']
-     }
-    }
-    ['PATCH:/todos/{todoId}']: {
-      params: {
-        todoId: string
+      'POST:/todos': {
+        body: Schemas['TodoAdd']
+        response: {
+          '204': void
+        }
       }
-      body: {
-        done?: boolean
-        title?: string
+      'GET:/todos/{todoId}': {
+        params: {
+          'todoId': string
+        }
+        response: {
+          'default': Schemas['Todo']
+        }
+      }
+      'PATCH:/todos/{todoId}': {
+        params: {
+          'todoId': string
+        }
+        body: {
+        'done'?: boolean
+        'title'?: string
+      }
+      }
+      'DELETE:/todos/{todoId}': {
+        params: {
+          'todoId': string
+        }
       }
     }
-    ['DELETE:/todos/{todoId}']: {
-      params: {
-        todoId: string
-      }
-    }
-  }
   }
 }
 
